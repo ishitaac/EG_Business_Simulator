9ce7035ad012e51db6a6c32b72369a28331bc10a
Ishita Choudhary <75246682+ishitaac@users.noreply.github.com>
2023-10-17 13:21:11 -0400

    Add files via upload

---
+++ location.py
@@ -0,0 +1,53 @@
+province = ['Ontario','Quebec','British Columbia','Alberta','Manitoba','Nova Scotia','Saskatchewan','Newfoundland and Labrador','New Brunswick','Prince Edward Island','Alberta / Saskatchewan','Ontario / Quebec']
+
+quebec = ['Montreal','Quebec City','Sherbrooke','Trois-Rivières','Chicoutimi – Jonquière','Saint-Jérôme','Saint-Jean-sur-Richelieu','Châteauguay','Drummondville','Granby','Beloeil','Saint-Hyacinthe','Joliette','Victoriaville','Salaberry-de-Valleyfield','Shawinigan','Rimouski','Sorel','Saint-Georges']
+
+ontario = ['Barrie,Belleville','Bolton','Bowmanville – Newcastle','Bradford','Brantford','Chatham','Cornwall','Georgetown','Guelph','Hamilton','Innisfil','Kanata','Keswick – Elmhurst Beach','Kingston','Kitchener','Leamington','London','Midland','Milton','North Bay','Orangeville','Orillia','Oshawa','Peterborough','Sarnia','Sault Ste. Marie','St. Catharines – Niagara Falls','St. Thomas','Stouffville','Stratford','Sudbury','Thunder Bay','Timmins','Toronto','Welland – Pelham','Windsor','Woodstock']
+
+alberta = ['Airdrie','Calgary','Cochrane','Edmonton','Fort McMurray','Fort Saskatchewan','Grande Prairie','Leduc','Lethbridge','Medicine Hat','Okotoks','Red Deer','Spruce Grove']
+
+alberta_Saskatchewan= ['Lloydminster']
+
+britishColumbia = ['Abbotsford','Campbell River','Chilliwack','Courtenay','Kamloops','Kelowna','Mission','Nanaimo','Parksville','Penticton','Prince George','Vancouver','Vernon','Victoria','White Rock']
+
+manitoba = ['Brandon,Winnipeg']
+
+new_Brunswick = ['Fredericton','Moncton','Saint John']
+
+newfoundland= ["St. John's"]
+
+novaScotia= ['Cape Breton – Sydney', 'Halifax']
+
+saskatchewan = ['Moose Jaw','Prince Albert','Regina,Saskatoon']
+
+princeEdward = ['Charlottetown']
+
+ontarioQuebec = ['Ottawa–Gatineau']
+
+
+# new dictionary: 
+
+locations :{
+    "alberta":[('Airdrie', '73,578'),('Fort McMurray', '68,002'),('Leduc', '33,505'),('Cochrane', '31,638'),('Okotoks', '30,214'),('Red Deer', '99,846'),('Spruce Grove', '39,348'),('Lethbridge', '92,563'),('Medicine Hat', '63,382'),('Fort Saskatchewan', '26,831'),('Grande Prairie', '63,172'),('Edmonton', '1,151,635'),('Calgary', '1,305,550')],
+
+    "alberta_Saskatchewan":[('Lloydminster', '31,582')],
+
+    "britishColumbia":[('Abbotsford', '132,300'),('Campbell River', '38,108'),('Chilliwack', '81,622'),('Courtenay', '48,917'),('Kamloops', '92,442'),('Kelowna', '181,380'),('Mission', '36,193'),('Nanaimo', '106,079'),('Parksville', '27,330'),('Penticton', '36,893'),('Prince George', '67,339'),('Vancouver', '2,426,160'),('Vernon', '51,896'),('Victoria', '363,222'),('White Rock', '109,167')],
+
+    "manitoba":[('Brandon', '50,532'),('Winnipeg', '758,515')],
+
+    "newBrunswick":[('Fredericton', '64,614'),('Moncton', '119,785'),('Saint John', '63,447')],
+
+    "newfoundland":[("St. John's", '185,565')],
+
+    "novaScotia":[('Cape Breton – Sydney', '30,960'),('Halifax', '348,634')],
+    "ontario":[('Barrie', '154,676'),('Belleville', '75,052'),('Bolton', '26,795'),('Bowmanville – Newcastle', '56,742'),('Bradford', '38,128'),('Brantford', '104,413'),('Chatham', '45,171'),('Cornwall', '47,286'),('Georgetown', '44,058'),('Guelph', '144,356'),('Hamilton', '729,560'),('Innisfil', '29,464'),('Kanata', '137,118'),('Keswick – Elmhurst Beach', '27,145'),('Kingston', '127,943'),('Kitchener', '522,888'),('Leamington', '35,730'),('London', '423,369'),('Midland', '26,246'),('Milton', '124,579'),('North Bay', '51,433'),('Orangeville', '34,177'),('Orillia', '33,379'),('Oshawa', '335,949'),('Peterborough', '84,793'),('Sarnia', '73,944'),('Sault Ste. Marie', '64,923'),('St. Catharines – Niagara Falls', '242,460'),('St. Thomas', '45,732'),('Stouffville', '36,753'),('Stratford', '32,878'),('Sudbury', '92,093'),('Thunder Bay', '95,266'),('Timmins', '28,874'),('Toronto', '5,647,656'),('Welland – Pelham', '69,302'),('Windsor', '306,519'),('Woodstock', '46,296')],
+
+    "ontarioQuebec":[('Ottawa–Gatineau', '1,068,821')],
+
+    "princeEdward":[('Charlottetown', '52,390')],
+
+    "quebec":[('Beloeil', '52,959'),('Châteauguay', '75,891'),('Chicoutimi – Jonquière', '103,934'),('Drummondville', '72,089'),('Granby', '62,624'),('Joliette', '49,246'),('Montreal', '3,675,219'),('Quebec City', '733,156'),('Rimouski', '38,708'),('Saint-Georges', '27,402'),('Saint-Hyacinthe', '50,616'),('Saint-Jean-sur-Richelieu', '88,083'),('Saint-Jérôme', '100,859'),('Salaberry-de-Valleyfield', '41,655'),('Shawinigan', '38,930'),('Sherbrooke', '151,157'),('Sorel', '36,650'),('Trois-Rivières', '128,057'),('Victoriaville', '46,322')],
+
+    "saskatchewan":[('Moose Jaw', '32,813'),('Prince Albert', '36,768'),('Regina', '224,996'),('Saskatoon', '264,637')]
+}
