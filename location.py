502b85bc944596ed6d4d1e5715e4215e087c2471
Daniel Chamberland-Tremblay <daniel.chamberland-tremblay@usherbrooke.ca>
2023-10-17 13:26:50 -0400

    Update location.py with number value of population

--- location.py
+++ location.py
@@ -28,26 +28,26 @@ ontarioQuebec = ['Ottawa–Gatineau']
 # new dictionary: 
 
 locations :{
-    "alberta":[('Airdrie', '73,578'),('Fort McMurray', '68,002'),('Leduc', '33,505'),('Cochrane', '31,638'),('Okotoks', '30,214'),('Red Deer', '99,846'),('Spruce Grove', '39,348'),('Lethbridge', '92,563'),('Medicine Hat', '63,382'),('Fort Saskatchewan', '26,831'),('Grande Prairie', '63,172'),('Edmonton', '1,151,635'),('Calgary', '1,305,550')],
+    "alberta":[('Airdrie', 73578),('Fort McMurray', 68002),('Leduc', 33505),('Cochrane', 31638),('Okotoks', 30214),('Red Deer', 99846),('Spruce Grove', 39348),('Lethbridge', 92563),('Medicine Hat', 63382),('Fort Saskatchewan', 26831),('Grande Prairie', 63172),('Edmonton', 1151635),('Calgary', 1305550)],
 
-    "alberta_Saskatchewan":[('Lloydminster', '31,582')],
+    "alberta_Saskatchewan":[('Lloydminster', 31582)],
 
-    "britishColumbia":[('Abbotsford', '132,300'),('Campbell River', '38,108'),('Chilliwack', '81,622'),('Courtenay', '48,917'),('Kamloops', '92,442'),('Kelowna', '181,380'),('Mission', '36,193'),('Nanaimo', '106,079'),('Parksville', '27,330'),('Penticton', '36,893'),('Prince George', '67,339'),('Vancouver', '2,426,160'),('Vernon', '51,896'),('Victoria', '363,222'),('White Rock', '109,167')],
+    "britishColumbia":[('Abbotsford', 132300),('Campbell River', 38108),('Chilliwack', 81622),('Courtenay', 48917),('Kamloops', 92442),('Kelowna', 181380),('Mission', 36193),('Nanaimo', 106079),('Parksville', 27330),('Penticton', 36893),('Prince George', 67339),('Vancouver', 2426160),('Vernon', 51896),('Victoria', 363222),('White Rock', 109167)],
 
-    "manitoba":[('Brandon', '50,532'),('Winnipeg', '758,515')],
+    "manitoba":[('Brandon', 50532),('Winnipeg', 758515)],
 
-    "newBrunswick":[('Fredericton', '64,614'),('Moncton', '119,785'),('Saint John', '63,447')],
+    "newBrunswick":[('Fredericton', 64614),('Moncton', 119785),('Saint John', 63447)],
 
-    "newfoundland":[("St. John's", '185,565')],
+    "newfoundland":[("St. John's", 185565)],
 
-    "novaScotia":[('Cape Breton – Sydney', '30,960'),('Halifax', '348,634')],
-    "ontario":[('Barrie', '154,676'),('Belleville', '75,052'),('Bolton', '26,795'),('Bowmanville – Newcastle', '56,742'),('Bradford', '38,128'),('Brantford', '104,413'),('Chatham', '45,171'),('Cornwall', '47,286'),('Georgetown', '44,058'),('Guelph', '144,356'),('Hamilton', '729,560'),('Innisfil', '29,464'),('Kanata', '137,118'),('Keswick – Elmhurst Beach', '27,145'),('Kingston', '127,943'),('Kitchener', '522,888'),('Leamington', '35,730'),('London', '423,369'),('Midland', '26,246'),('Milton', '124,579'),('North Bay', '51,433'),('Orangeville', '34,177'),('Orillia', '33,379'),('Oshawa', '335,949'),('Peterborough', '84,793'),('Sarnia', '73,944'),('Sault Ste. Marie', '64,923'),('St. Catharines – Niagara Falls', '242,460'),('St. Thomas', '45,732'),('Stouffville', '36,753'),('Stratford', '32,878'),('Sudbury', '92,093'),('Thunder Bay', '95,266'),('Timmins', '28,874'),('Toronto', '5,647,656'),('Welland – Pelham', '69,302'),('Windsor', '306,519'),('Woodstock', '46,296')],
+    "novaScotia":[('Cape Breton – Sydney', 30960),('Halifax', 348634)],
+    "ontario":[('Barrie', 154676),('Belleville', 75052),('Bolton', 26795),('Bowmanville – Newcastle', 56742),('Bradford', 38128),('Brantford', 104413),('Chatham', 45171),('Cornwall', 47286),('Georgetown', 44058),('Guelph', 144356),('Hamilton', 729560),('Innisfil', 29464),('Kanata', 137118),('Keswick – Elmhurst Beach', 27145),('Kingston', 127943),('Kitchener', 522888),('Leamington', 35730),('London', 423369),('Midland', 26246),('Milton', 124579),('North Bay', 51433),('Orangeville', 34177),('Orillia', 33379),('Oshawa', 335949),('Peterborough', 84793),('Sarnia', 73944),('Sault Ste. Marie', 64923),('St. Catharines – Niagara Falls', 242460),('St. Thomas', 45732),('Stouffville', 36753),('Stratford', 32878),('Sudbury', 92093),('Thunder Bay', 95266),('Timmins', 28874),('Toronto', 5647656),('Welland – Pelham', 69302),('Windsor', 306519),('Woodstock', 46296)],
 
-    "ontarioQuebec":[('Ottawa–Gatineau', '1,068,821')],
+    "ontarioQuebec":[('Ottawa–Gatineau', 1068821)],
 
-    "princeEdward":[('Charlottetown', '52,390')],
+    "princeEdward":[('Charlottetown', 52390)],
 
-    "quebec":[('Beloeil', '52,959'),('Châteauguay', '75,891'),('Chicoutimi – Jonquière', '103,934'),('Drummondville', '72,089'),('Granby', '62,624'),('Joliette', '49,246'),('Montreal', '3,675,219'),('Quebec City', '733,156'),('Rimouski', '38,708'),('Saint-Georges', '27,402'),('Saint-Hyacinthe', '50,616'),('Saint-Jean-sur-Richelieu', '88,083'),('Saint-Jérôme', '100,859'),('Salaberry-de-Valleyfield', '41,655'),('Shawinigan', '38,930'),('Sherbrooke', '151,157'),('Sorel', '36,650'),('Trois-Rivières', '128,057'),('Victoriaville', '46,322')],
+    "quebec":[('Beloeil', 52959),('Châteauguay', 75891),('Chicoutimi – Jonquière', 103934),('Drummondville', 72089),('Granby', 62624),('Joliette', 49246),('Montreal', 3675219),('Quebec City', 733156),('Rimouski', 38708),('Saint-Georges', 27402),('Saint-Hyacinthe', 50616),('Saint-Jean-sur-Richelieu', 88083),('Saint-Jérôme', 100859),('Salaberry-de-Valleyfield', 41655),('Shawinigan', 38930),('Sherbrooke', 151157),('Sorel', 36650),('Trois-Rivières', 128057),('Victoriaville', 46322)],
 
-    "saskatchewan":[('Moose Jaw', '32,813'),('Prince Albert', '36,768'),('Regina', '224,996'),('Saskatoon', '264,637')]
+    "saskatchewan":[('Moose Jaw', 32813),('Prince Albert', 36768),('Regina', 224996),('Saskatoon', 264637)]
 }
