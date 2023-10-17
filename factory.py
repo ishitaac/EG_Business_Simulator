e260bc83d996a077f0ecdfda20043ccb96b501d6
Ishita Choudhary <75246682+ishitaac@users.noreply.github.com>
2023-10-17 13:36:40 -0400

    Update factory.py

--- factory.py
+++ factory.py
@@ -10,8 +10,10 @@ import string
 from factory import SubFactory
 from datetime import datetime, timedelta
 from factory.fuzzy import FuzzyDecimal
-
-
+from django.db import connection
+import worker
+from data.location import *
+from faker.generator import Generator
 FAKE = faker.Faker()
 
 
@@ -54,60 +56,240 @@ class CustomFakerProvider:
         return f"{first_name}.{last_name}@{domain}"
     
 # Custom Provider FOR CITIES
-class CustomFakerProvider:
-    def postalCode(self, country):
-        if country.lower() == 'canada':
-            return random.choice(['A1A 1A1', 'B2B 2B2', 'C3C 3C3', 'D4D 4D4', 'E5E 5E5'])
-        elif country.lower() == 'usa':
-            return random.choice(['12345', '54321', '67890', '98765', '45678'])
+class CustomFakerProvider(BaseProvider):
+    # def postalCode(self, country):
+    #     if country.lower() == 'canada':
+    #         return random.choice(['A1A 1A1', 'B2B 2B2', 'C3C 3C3', 'D4D 4D4', 'E5E 5E5'])
+    #     elif country.lower() == 'usa':
+    #         return random.choice(['12345', '54321', '67890', '98765', '45678'])
+    
+    def city(self, provience):
+        result = random.choices([i[0] for i in locations[provience]], [i[1] for i in locations[provience]])
+        return result
+    
 
-    def city(self, country):
-        if country.lower() == 'canada':
-            return random.choice(['Toronto', 'Montreal', 'Vancouver', 'Calgary', 'Ottawa'])
-        elif country.lower() == 'usa':
-            return random.choice(['New York', 'Los Angeles', 'Chicago', 'Houston', 'Miami'])
+        # random.choice(i[0] for i in locations[provience])
+    # def population
+    # def population(self, provience):
+    #     return random.choices([i[0] for i in locations[provience]], weights=[i[1] for i in locations[provience]], k=1)[0]
 
-    def state(self, city):
-        if city.lower() in ['toronto', 'ottawa']:
-            return 'Ontario'
-        elif city.lower() =="montreal":
-            return 'Quebec'
-        elif city.lower() == 'calgary':
-            return 'Alberta'
-        elif city.lower() == 'vancouver':
-            return 'British Columbia'
-        elif city.lower() == 'miami':
-            return 'Florida'
-        elif city.lower() == 'new york':
-            return 'New York state'
-        elif city.lower() == 'chicago':
-            return 'Illnois'
-        elif city.lower() == 'los angeles':
-            return 'California'
-        elif city.lower() == 'houston':
-            return 'Texas'
+    # def city(self, provience):
+    #     if provience.lower() == 'ontario':
+    #         return random.choice(ontario)
+    #     elif provience.lower() == 'quebec':
+    #         return random.choice(quebec)
+    #     elif provience.lower() == 'alberta':
+    #         return random.choice(alberta)
+    #     elif provience.lower() == 'british columbia':
+    #         return random.choice(britishColumbia)
+    #     elif provience.lower() == 'manitoba':
+    #         return random.choice(manitoba)
+    #     elif provience.lower() == 'nova scotia':
+    #         return random.choice(novaScotia)
+    #     elif provience.lower() == 'Saskatchewan':
+    #         return random.choice(saskatchewan)
+    #     elif provience.lower() == 'newfoundland and labrador':
+    #         return random.choice(newfoundland)
+    #     elif provience.lower() == 'new brunswick':
+    #         return random.choice(new_Brunswick)
+    #     elif provience.lower() == 'prince edward island':
+    #         return random.choice(princeEdward)
+    #     elif provience.lower() == 'Alberta / Saskatchewan':
+    #         return random.choice(alberta_Saskatchewan)
+    #     elif provience.lower() == 'ontario / quebec':
+    #         return random.choice(ontarioQuebec)
+            
+            
+
+    # without writing so much code: 
+    # def __init__(self, generator: Generator):
+    #     super().__init__(generator)
+    #     self.generated_cities = {}
+
+    # def city(self, province):
+    #         if province in self.generated_cities:
+    #             return self.generated_cities[province]
+    #         else:
+    #             return f"No city generated for {province}"
+
+    # def generate_city(self, province):
+    #         if province.lower() == 'ontario':
+    #             self.generated_cities[province] = random.choice(ontario)
+    #         elif province.lower() == 'quebec':
+    #             self.generated_cities[province] = random.choice(quebec)
+    #         elif province.lower() == 'alberta':
+    #             self.generated_cities[province] = random.choice(alberta)
+    #         elif province.lower() == 'british columbia':
+    #             self.generated_cities[province] = random.choice(britishColumbia)
+    #         elif province.lower() == 'manitoba':
+    #             self.generated_cities[province] = random.choice(manitoba)
+    #         elif province.lower() == 'nova scotia':
+    #             self.generated_cities[province] = random.choice(novaScotia)
+    #         elif province.lower() == 'saskatchewan':
+    #             self.generated_cities[province] = random.choice(saskatchewan)
+    #         elif province.lower() == 'newfoundland and labrador':
+    #             self.generated_cities[province] = random.choice(newfoundland)
+    #         elif province.lower() == 'new brunswick':
+    #             self.generated_cities[province] = random.choice(new_Brunswick)
+    #         elif province.lower() == 'prince edward island':
+    #             self.generated_cities[province] = random.choice(princeEdward)
+    #         elif province.lower() == 'alberta / saskatchewan':
+    #             self.generated_cities[province] = random.choice(alberta_Saskatchewan)
+    #         elif province.lower() == 'ontario / quebec':
+    #             self.generated_cities[province] = random.choice(ontarioQuebec)
+    #         else:
+    #             return f"No data for province {province}"
+
+    #         return self.generated_cities[province]
+    
+    # def population(self, province):
+    #     generated_city = self.city(province)
+        
+    #     if generated_city != f"No city generated for {province}":
+    #         city_data = locations.get(province, [])
+    #         for city_tuple in city_data:
+    #             if city_tuple[0] == generated_city:
+    #                 return city_tuple[1]
+            
+    #         return f"No data for province {province} and city {generated_city}"
+        
+    #     return f"No data for province {province}"
+
+    
+    
+
+
+
+
+    # def population(self,city):
+    #     if provience.lower() == 'ontario':
+    #         return random.choice(ontario)
+    #     elif provience.lower() == 'quebec':
+    #         return random.choice(quebec)
+    #     elif provience.lower() == 'alberta':
+    #         return random.choice(alberta)
+    #     elif provience.lower() == 'british columbia':
+    #         return random.choice(britishColumbia)
+    #     elif provience.lower() == 'manitoba':
+    #         return random.choice(manitoba)
+    #     elif provience.lower() == 'nova scotia':
+    #         return random.choice(novaScotia)
+    #     elif provience.lower() == 'Saskatchewan':
+    #         return random.choice(saskatchewan)
+    #     elif provience.lower() == 'newfoundland and labrador':
+    #         return random.choice(newfoundland)
+    #     elif provience.lower() == 'new brunswick':
+    #         return random.choice(new_Brunswick)
+    #     elif provience.lower() == 'prince edward island':
+    #         return random.choice(princeEdward)
+    #     elif provience.lower() == 'Alberta / Saskatchewan':
+    #         return random.choice(alberta_Saskatchewan)
+    #     elif provience.lower() == 'ontario / quebec':
+    #         return random.choice(ontarioQuebec)
+
+    # def city(self, country):
+    #     if country.lower() == 'canada':
+    #         return random.choice(['Toronto', 'Montreal', 'Vancouver', 'Calgary', 'Ottawa'])
+    #     elif country.lower() == 'usa':
+    #         return random.choice(['New York', 'Los Angeles', 'Chicago', 'Houston', 'Miami'])
+
+    # def state(self, city):
+    #     if city.lower() in ['toronto', 'ottawa']:
+    #         return 'Ontario'
+    #     elif city.lower() =="montreal":
+    #         return 'Quebec'
+    #     elif city.lower() == 'calgary':
+    #         return 'Alberta'
+    #     elif city.lower() == 'vancouver':
+    #         return 'British Columbia'
+    #     elif city.lower() == 'miami':
+    #         return 'Florida'
+    #     elif city.lower() == 'new york':
+    #         return 'New York state'
+    #     elif city.lower() == 'chicago':
+    #         return 'Illnois'
+    #     elif city.lower() == 'los angeles':
+    #         return 'California'
+    #     elif city.lower() == 'houston':
+    #         return 'Texas'
 
 
 
 # Location Factory
-# class LocationFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = Location
-
-#     country = factory.Faker('random_element', elements=['Canada', 'USA'])
-#     postalCode = factory.LazyAttribute(lambda obj: CustomFakerProvider().postalCode(obj.country))
-#     city = factory.LazyAttribute(lambda obj: CustomFakerProvider().city(obj.country))
-#     state = factory.LazyAttribute(lambda obj: CustomFakerProvider().state(obj.city))
-# previous code
-
-class LocationFaction(factory.django.DjangoModelFactory):
+class LocationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Location
+
+    # country = factory.Faker('random_element', elements=['Canada', 'USA'])
+    # postalCode = factory.LazyAttribute(lambda obj: CustomFakerProvider().postalCode(obj.country))
+    # city = factory.LazyAttribute(lambda obj: CustomFakerProvider().city(obj.country))
+    # state = factory.LazyAttribute(lambda obj: CustomFakerProvider().state(obj.city))
+
+    provience = factory.Faker('random_element', elements= province)
+    city = factory.LazyAttribute(lambda obj: CustomFakerProvider().city(obj.provience))
+    population = factory.LazyAttribute(lambda obj: CustomFakerProvider().city(obj.provience))
+    # # # population = factory.Faker('random_int', min=100000, max=10000000)
+    # @factory.lazy_attribute
+    # def city(self):
+    #     return CustomFakerProvider(self.generator).city(self.provience)
+
+    # @factory.lazy_attribute
+    # def population(self):
+    #     return CustomFakerProvider(self.generator).city_population(self.provience)
+
+    # @classmethod
+    # def _get_generator(cls):
+    #     return cls._meta.declarations.get('generator', None)
+
+
+# class LocationFactory(factory.django.DjangoModelFactory):
+#     class Meta:
+
+#         model = Location
     
-    city = factory.Faker('city')
-    province = factory.Faker('state')
-    population = factory.Faker('random_int', min=1000, max=1000000)
-    
+
+#     @classmethod
+#     def create(cls):
+        # Execute the SQL query to fetch location data
+        # with connection.cursor() as cursor:
+        #     cursor.execute("SELECT Population_Center, Province, Population_2021 FROM mdm.raw_wikipedia_canada_cities")
+        #     location_data = cursor.fetchall()
+        # if __name__ == "__main__":
+        #     stmt = """
+        #     # SELECT Populta FROM raw_wikipedia_canada_cities;
+        #     `Population center` AS population_center, `Province` AS province, `Size Group` AS size_group
+        #     """
+        #     import mysql.connector
+        #     from mysql.connector import errorcode
+
+        #     try:
+        #         cnx = mysql.connector.connect(host='gta-ins07.fadm.usherbrooke.ca', port='3307', user='choi1602', password='L5_lb*_NLTK',
+        #                                         database='mdm')
+        #         cur = cnx.cursor()
+        #         msg = cur.execute(stmt)
+        #         rs = cur.fetchall()
+        #         # for i in rs:
+        #         #     print(i)
+
+        #     except mysql.connector.Error as err:
+        #         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        #             print("Something is wrong with your user name or password")
+        #         elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        #             print("Database does not exist")
+        #         else:
+        #             print(err)
+        #     else:
+        #         cnx.close()
+        
+        # using the exact module from the worker factory - get the fetched data
+
+        # Create a Location instance using the fetched data
+        # return cls(
+        #     city=rs[0],
+        #     provience=rs[1],
+        #     population=rs[2],
+        # )
+
 
 # DEPARTMENT FACTORY
 
@@ -123,6 +305,7 @@ DEPARTMENT_CODES = {
     "Data Science": "DATA-606",
     "Legal & Complaince": "LEGAL-707",
 }
+
 class DepartmentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Department
@@ -130,7 +313,7 @@ class DepartmentFactory(factory.django.DjangoModelFactory):
     name = factory.Iterator(DEPARTMENT_CODES.keys())
     department_code = factory.LazyAttribute(lambda obj: DEPARTMENT_CODES[obj.name])
 
-
+# JOB FACTORY
 class JobFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Job
@@ -321,32 +504,30 @@ class EmployeeFactory(factory.django.DjangoModelFactory):
 from factory import SubFactory
 from .models import Manager
 
-# class ManagerFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = Manager
+from factory import SubFactory, LazyAttribute
+from .models import Manager, Employee, Department
 
-#     # You can use a SubFactory to create an Employee and use its data for the Manager
-#     employee = factory.SubFactory(EmployeeFactory)  # Assuming EmployeeFactory is defined correctly
-#     first_name = factory.SelfAttribute('employee.first_name')
-#     last_name = factory.SelfAttribute('employee.last_name')
-#     email = factory.SelfAttribute('employee.email')
-#     department = factory.Iterator(Department.objects.all())  # Choose a department for the manager
+class ManagerFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Manager
 
-#     # If you want to ensure the manager has the same employee ID as the associated employee,
-#     # you can do it like this:
-#     @classmethod
-#     def create(cls, **kwargs):
-#         manager = super().create(**kwargs)
-#         manager.employee_id = manager.employee.employee_id
-#         manager.save()
-#         return manager
+    # Use LazyAttribute to assign an existing employee to each manager record
+    employee = LazyAttribute(lambda obj: Employee.objects.order_by('?').first())
 
-# # class ManagerFactory(factory.django.DjangoModelFactory):
-# #     class Meta:
-# #         model = Manager
+    # Use LazyAttribute to ensure only one manager is created per department
+    department = LazyAttribute(lambda obj: Department.objects.order_by('?').first())
 
-# #     employee = factory.SubFactory(EmployeeFactory)
-# #     department = factory.SubFactory(DepartmentFactory)
+    @factory.lazy_attribute
+    def first_name(self):
+        return self.employee.first_name
+
+    @factory.lazy_attribute
+    def last_name(self):
+        return self.employee.last_name
+
+    @factory.lazy_attribute
+    def email(self):
+        return self.employee.email
 
 class PerformanceFactory(factory.django.DjangoModelFactory):
     class Meta:
